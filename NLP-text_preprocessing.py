37c6d2a1db39df4f8d0005c4ead17cb01f9c1992
Yusuf Büyükdağ <123562650+ybuyukdag@users.noreply.github.com>
2023-06-06 10:59:04 +0300

    Update NLP-text_preprocessing.py

--- NLP-text_preprocessing.py
+++ NLP-text_preprocessing.py
@@ -1,7 +1,7 @@
 ##TOKENIZATION(Bölütleme)
 import nltk
 text = "Metin önişleme dil işleme modelleri için önemli bir adımdır."
-#print(nltk.word_tokenize(text))
+print(nltk.word_tokenize(text))
 '''
 >>>['Metin', 'önişleme', 'dil', 'işleme', 'modelleri', 'için', 'önemli', 'bir', 'adımdır', '.']
 '''
@@ -9,7 +9,7 @@ text = "Metin önişleme dil işleme modelleri için önemli bir adımdır."
 ##TEXT CLEANING(Metin Temizleme)
 import string
 msg = 'Dil işlemede kullanılan kütüphaneler: nltk, spacy, scikit-learn vb.dir'
-#print(msg.translate(str.maketrans('','', string.punctuation))) #Noktalama temizleme
+print(msg.translate(str.maketrans('','', string.punctuation))) #Noktalama temizleme
 '''
 >>>Dil işlemede kullanılan kütüphaneler nltk spacy scikitlearn vbdir
 '''
@@ -17,14 +17,14 @@ stop_words = ["acaba", "ve", "bir", "birçok", "ama", "için"]
 msg = "Acaba metindeki dolgu kelimelerini bulmak ve temizlemek için ne yapmalıyım?"
 s1 = set(stop_words)
 s2 = set(msg.lower().split())
-#print(s1.intersection(s2)) #önceden belirtilen Gereksiz sözcükleri temizleme
+print(s1.intersection(s2)) #önceden belirtilen Gereksiz sözcükleri temizleme
 '''
 >>>{'acaba', 've', 'için'}
 '''
 
 ##TEXT NORMALIZATION(Metin Normalizasyonu)
 msg = "Acaba metindeki dolgu kelimelerini bulmak ve temizlemek için ne yapmalıyım?"
-#print(msg.lower()) #Tekrarlardan kaçınmak için harfleri aynı boyuta getirmek, Küçük harf
+print(msg.lower()) #Tekrarlardan kaçınmak için harfleri aynı boyuta getirmek, Küçük harf
 '''
 >>>acaba metindeki dolgu kelimelerini bulmak ve temizlemek için ne yapmalıyım?
 '''
