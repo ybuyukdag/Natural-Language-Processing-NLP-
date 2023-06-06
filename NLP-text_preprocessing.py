e404b1795d8c50be5a51ba64158fddfa8183908e
Yusuf Büyükdağ <123562650+ybuyukdag@users.noreply.github.com>
2023-06-06 10:33:20 +0300

    Basic work with NLTK-SpaCy
    This work contains a collection of text preprocessing steps that are common in a variety of NLP tasks. The steps include:
    
    Text cleaning: This involves removing punctuation, stopwords, numbers, HTML tags, URLs, emojis, and whitespace.
    Replacing accented characters: This converts accented characters to their unaccented counterparts.
    Text normalization: This involves converting all text to lowercase and removing diacritics.
    Spell corrections: This corrects common spelling errors.
    Tokenization: This breaks text into words or sentences.
    Lemmatization: This reduces words to their base form.
    Stemming: This reduces words to their root form.
    The preprocessing steps can implement using a variety of Python libraries, including string, re, nltk, spaCy, gensim, textblob, unidecode, pyspellchecker, and autocorrect.
    
    This work is intended to be a starting point for anyone who is interested in learning about text preprocessing or who needs to perform text preprocessing for an NLP task.

---
+++ NLP-text_preprocessing.py
@@ -0,0 +1,60 @@
+##TOKENIZATION(Bölütleme)
+import nltk
+text = "Metin önişleme dil işleme modelleri için önemli bir adımdır."
+#print(nltk.word_tokenize(text))
+'''
+>>>['Metin', 'önişleme', 'dil', 'işleme', 'modelleri', 'için', 'önemli', 'bir', 'adımdır', '.']
+'''
+
+##TEXT CLEANING(Metin Temizleme)
+import string
+msg = 'Dil işlemede kullanılan kütüphaneler: nltk, spacy, scikit-learn vb.dir'
+#print(msg.translate(str.maketrans('','', string.punctuation))) #Noktalama temizleme
+'''
+>>>Dil işlemede kullanılan kütüphaneler nltk spacy scikitlearn vbdir
+'''
+stop_words = ["acaba", "ve", "bir", "birçok", "ama", "için"]
+msg = "Acaba metindeki dolgu kelimelerini bulmak ve temizlemek için ne yapmalıyım?"
+s1 = set(stop_words)
+s2 = set(msg.lower().split())
+#print(s1.intersection(s2)) #önceden belirtilen Gereksiz sözcükleri temizleme
+'''
+>>>{'acaba', 've', 'için'}
+'''
+
+##TEXT NORMALIZATION(Metin Normalizasyonu)
+msg = "Acaba metindeki dolgu kelimelerini bulmak ve temizlemek için ne yapmalıyım?"
+#print(msg.lower()) #Tekrarlardan kaçınmak için harfleri aynı boyuta getirmek, Küçük harf
+'''
+>>>acaba metindeki dolgu kelimelerini bulmak ve temizlemek için ne yapmalıyım?
+'''
+
+#Kuraldışı kelimelerin Düzeltilmesi(Conversion of non-canonical words)
+#Yazım hataları(Spellcheck)
+
+##STEMMING AND LEMMATIZATION(Gövdeleme ve Kök Bulma )
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+ps = PorterStemmer()
+# choose some words to be stemmed
+words = ["program", "programs", "programmer", "programming", "programmers"]
+for w in words:
+	print(w, " : ", ps.stem(w))
+'''
+>>>program  :  program
+programs  :  program
+programmer  :  programm
+programming  :  program
+programmers  :  programm
+'''
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+print("rocks :", lemmatizer.lemmatize("rocks"))
+print("corpora :", lemmatizer.lemmatize("corpora"))
+# a denotes adjective in "pos"
+print("better :", lemmatizer.lemmatize("better", pos ="a"))
+'''
+>>>rocks : rock
+corpora : corpus
+better : good
+'''
